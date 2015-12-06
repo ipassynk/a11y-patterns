85fc574694fac8fd2d68a23b56093ec445385618
Julia.Passynkova <julia.passynkova@nurun.com>
2015-12-06 14:08:14 -0500

    dec 06 2015

--- src/app/index.module.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('routeA11y', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria',
-      'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
